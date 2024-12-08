3b2032e3fdd73989ade5949c8d55d102d46da2e1
oliver <190023113+0xRobinman@users.noreply.github.com>
2024-12-08 18:44:00 +0000

    PPU progress

---
+++ app/src/main/java/org/emulator/ppu/Memory.java
@@ -0,0 +1,91 @@
+package org.emulator.ppu;
+
+import org.emulator.memory.Ram;
+
+public class Memory {
+    
+    private final static int OAM_SIZE = 256;
+    public static final int VRAM_SIZE = 0x4000;
+
+    private final static short VRAM_START = 0x0000;
+    private final static short VRAM_END = 0x3FFF;
+    
+    public static final short PATTERN_TABLE_START = 0x0000;
+    public static final short PATTERN_TABLE_END = 0x1FFF;
+
+    public static final short NAME_TABLE_0_START = 0x2000;
+    public static final short NAME_TABLE_0_END = 0x23FF;
+    
+    public static final short NAME_TABLE_1_START = 0x2400;
+    public static final short NAME_TABLE_1_END = 0x27FF;
+    
+    public static final short NAME_TABLE_2_START = 0x2800;
+    public static final short NAME_TABLE_2_END = 0x2BFF;
+    
+    public static final short NAME_TABLE_3_START = 0x2C00;
+    public static final short NAME_TABLE_3_END = 0x2FFF;
+    
+    public static final short PALETTE_START = 0x3F00;
+    public static final short PALETTE_END = 0x3F1F;
+    
+    public static final short MIRRORED_PALETTE_START = 0x3F20;
+    public static final short MIRRORED_PALETTE_END = 0x3FFF;
+       
+    public static final short NAME_TABLE_MIRROR_START = 0x3000;
+    public static final short NAME_TABLE_MIRROR_END = 0x3EFF;
+        
+    private static byte [] vRam = new byte[0x3FFF];
+
+
+
+    public static byte readPatternMemory(short offset) {
+        return Ram.read((short)(PATTERN_TABLE_START + offset));
+    }
+
+    public static byte readPatternMemory(byte offset) {
+        return Ram.read((byte)(PATTERN_TABLE_START + offset));
+    }
+
+
+    public static byte readPaletteMemory(short offset) {
+        return Ram.read((short)(PALETTE_START + offset));
+    }
+
+    public static byte readPaletteMemory(byte offset) {
+        return Ram.read((byte)(PALETTE_START + offset));
+    }
+
+
+    public static byte readNameTableMemory(short offset) {
+        return Ram.read((short)(NAME_TABLE_0_START + offset));
+    }
+
+    public static byte readNameTableMemory(byte offset) {
+        return Ram.read((byte)(NAME_TABLE_0_START + offset));
+    }
+
+
+
+    public static byte[] getOAM() {
+        byte [] oam = new byte[OAM_SIZE];
+        byte oamAddress = Registers.getOAMAddressRegister();
+        for (int i = 0; i < OAM_SIZE; i++) {
+            oam[i] = Ram.read((short)(i+oamAddress));
+        }
+        return oam;
+    }
+
+
+    public static byte readNameTable(short offset) {
+        return Ram.read((short)(NAME_TABLE_0_START + offset));
+    }
+
+    public static byte[] getVRAM() {
+        vRam = new byte[0x3FFF];
+        for (short i = VRAM_START; i < VRAM_END; i++) {
+            vRam[i] = Ram.read(i);
+        }
+        return vRam;
+    }
+
+}
--- app/src/main/java/org/emulator/ppu/Ppu.java
+++ app/src/main/java/org/emulator/ppu/Ppu.java
@@ -1,6 +1,7 @@
 package org.emulator.ppu;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
@@ -8,21 +9,74 @@ import javax.swing.JPanel;
  * Picture processing unit
  */
 public class Ppu extends JPanel {
+    private static final int LINES = 262;
+    private static final int CYCLES = 341;
+    private static final int VISIBLE_LINE_START = 0;
+    private static final int VISIBLE_LINE_END = 240;
+    private static final int SPRITE_START = 256;
+    private static final int SPRITE_END = 240;
+    private static final int VBLANK_START = 241;
+    private static final int VBLANK_END = 260;
+    private static final int SCREEN_HEIGHT = 240; // NES resolution height
+    private static final int SCREEN_WIDTH = 256;
+
+    BufferedImage frameBuffer;
+    private int cycle = 0, line = 0;
+
+    public Ppu() {
+        frameBuffer = new BufferedImage(SCREEN_WIDTH, SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+    }
+
+    private void handleScanLineEnd() {
+
+    }
+
+    private void handleVisibleScanLines() {
+    }
 
 
-    private final int MAX_ADDRESS = 0x3FFF;
+    private void handlePreRenderLine() {
+
+    }
+
+    private void handleVBlank() {
+
+    }
 
 
-    public Ppu() {}
+    private void ppuTick() {
+        cycle++;
+        renderPixel();
+        handleVBlank();
+        handleScanLineEnd();
+        handleVisibleScanLines();
+        handlePreRenderLine();
+    }
 
-    
+    private void ppuCycle() {
+        for (int i = 0; i < CYCLES; i++)
+            ppuTick();
+    }
 
 
+    private int fetchSpritePixel(int x, int y) {
+
+        return 0;
+    }
+
+    private int fetchBackgroundPixel(int x, int y) {
+        return 0;
+    }
+
+    private void renderPixel() {
+
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+        g.drawImage(frameBuffer, 0, 0, getWidth(), getHeight(), null);
+        ppuCycle();
     }
 
 
